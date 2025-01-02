233bbd606fdb045d69eb263699d86d0035a66841
plastic <promotionplastic13@gmail.com>
2025-01-02 23:28:04 +0530

    styling for the home page

---
+++ script.js
@@ -0,0 +1,16 @@
+let hiddentable1 = document.querySelectorAll('.hiddentable1')
+let hiddentable2 = document.querySelectorAll('.hiddentable2')
+let hiddentable3 = document.querySelectorAll('.hiddentable3')
+let hiddentable4 = document.querySelectorAll('.hiddentable4')
+let hiddentable5 = document.querySelectorAll('.hiddentable5')
+let hiddentable6 = document.querySelectorAll('.hiddentable6')
+let hiddentable7 = document.querySelectorAll('.hiddentable7')
+let hiddentable8 = document.querySelectorAll('.hiddentable8')
+
+let week = Document.querySelectorAll('.week')
+
+week.forEach(element => {
+    
+});
+
+
